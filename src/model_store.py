a3a882a60860ded5c6fdf91ea78594b3321720bd
katoro8989 <107518964+katoro8989@users.noreply.github.com>
2024-03-09 19:27:01 +0800

    Update model_store.py

--- src/model_store.py
+++ src/model_store.py
@@ -25,13 +25,13 @@ def get_model(hparams:dict):
 class CNN(nn.Module):
     def __init__(self, input_shape, n_classes):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(input_shape[0], 16, 5, padding=2)
-        self.conv2 = nn.Conv2d(16, 64, 5, padding=2)
-        self.conv3 = nn.Conv2d(64, 128, 5, padding=2)
+        self.conv1 = nn.Conv2d(input_shape[0], 16, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, padding=2) 
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
         
         self.pool = nn.MaxPool2d(2, 2)
         
-        self.fc1 = nn.Linear(128 * 5 * 5, 120)
+        self.fc1 = nn.Linear(64 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, n_classes)
         
